6264b56bbf492c1bbec77d33725e68a103dca759
Rahul Jaiswal <extern.rahul.jaiswal@vw.com>
2020-04-08 23:10:05 -0400

    Second commit

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const init = async() => {
             return "Hello World!";
         }
     });
+    /*
+    var polyglot = new Polyglot({locale: "en"});
+    polyglot.extend({
+        "hello": "Hello",
+        "hello_name": "Hello, %{name}",
+        "foo": {"bar": "This phrase's key is foo.bar."}
+    });
+    console.log("Polyglot locale is - " + polyglot.locale());
+    console.log(polyglot.t("hello_name", {name: "Rahul"}));
+*/
 
     await server.start();
     console.log("Server is running on %s", server.info.uri);
@@ -21,5 +31,7 @@ process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
+
+
 
 init();
